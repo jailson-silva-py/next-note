8f8ee6c0d6faf7dda607f2ddd5ed5f60471a1c4c
CruelSun0 <jailsonsunshime@gmail.com>
2025-10-14 10:47:39 -0300

    Removendo redundância

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  env: {
-
-    AUTH_SECRET:process.env.AUTH_SECRET,
-    DATABASE_URL:process.env.DATABASE_URL,
-    AUTH_GITHUB_SECRET:process.env.GITHUB_SECRET,
-    AUTH_DISCORD_ID:process.env.AUTH_DISCORD_ID,
-    AUTH_DISCORD_SECRET:process.env.AUTH_DISCORD_SECRET,
-    API_KEY:process.env.API_KEY,
-    API_URL:process.env.API_URL
-    
-
-  },
-
   experimental: {
 
     serverActions:{
